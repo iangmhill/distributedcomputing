a864c290f126df969270230a6b41a160350dfbe6
Ian Hill <ian.hill@students.olin.edu>
2017-02-04 18:58:19 -0500

    Count words into histogram

--- assignment1/task1.py
+++ assignment1/task1.py
@@ -1,14 +1,24 @@
 from threading import Thread
 from os import listdir
+from re import sub, split
 
 class CountWords(Thread):
   def __init__(self):
     super(CountWords, self).__init__()
 
   def run(self):
-    files = listdir("./lib")
-    print(files)
-
+    hist = {}
+    texts = listdir('./lib')
+    for text in texts:
+      with open('./lib/' + text) as book:
+        for line in book.readlines():
+          only_words = sub(r'[^a-zA-Z\s]+', ' ', line)
+          split_words = split(r'\s+', only_words.strip())
+          for split_word in split_words:
+            word = split_word.lower()
+            hist[word] = hist.get(word, 0) + 1
+        print(hist)
+        return
 
 def main():
   word_counter = CountWords()
