c440ac012cea705b928c8a787f0c6ca61c9045ed
Ian Hill <ian.hill@students.olin.edu>
2017-02-04 18:21:39 -0500

    Can list files in lib directory

--- assignment1/task1.py
+++ assignment1/task1.py
@@ -1,1 +1,18 @@
-import threading
+from threading import Thread
+from os import listdir
+
+class CountWords(Thread):
+  def __init__(self):
+    super(CountWords, self).__init__()
+
+  def run(self):
+    files = listdir("./lib")
+    print(files)
+
+
+def main():
+  word_counter = CountWords()
+  word_counter.start()
+
+if __name__ == '__main__':
+  main()
